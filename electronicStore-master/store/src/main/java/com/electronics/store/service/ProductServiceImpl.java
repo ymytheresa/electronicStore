dda774f16df6c2bb02663dfdfb104d352717e63e
Theresa <ymy.theresa@gmail.com>
2023-11-15 00:06:29 +0800

    added todo for reminding better update logics in future

--- electronicStore-master/store/src/main/java/com/electronics/store/service/ProductServiceImpl.java
+++ electronicStore-master/store/src/main/java/com/electronics/store/service/ProductServiceImpl.java
@@ -24,5 +24,29 @@ public class ProductServiceImpl {
         }
     }
 
+    public Product getProduct(Integer productId){
+        Optional<Product> productOptional = productRepository.findById(productId);
+        if(productOptional.isPresent()){
+            return productOptional.get();
+        }
+        return null; //todo, throw exception
+    }
+
+    public List<Product> getAllProducts(){
+        return this.productRepository.findAll();
+    }
+
+    public Product updateProduct(Product product){
+        //todo, revisit this and discountrule update logic, like here should be find by id and rule
+        Optional<Product> productOptional = productRepository.findById(product.getProductId());
+        if(productOptional.isPresent()){
+            productRepository.deleteById(product.getProductId()); //todo, replace instead of remove then add later
+            return productRepository.save(product);
+        }
+        return null; //todo, throw exception
+    }
+
+
+
 }
 
